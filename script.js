674d092cc6778c5be159da16263650d5f15ef0af
Christian <christian.quinlan36@gmail.com>
2020-12-09 14:24:50 +0000

    added authors version of expanding cards function

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
+
 const panels =document.querySelectorAll(".panel");
+// ****MY VERSION*****
+// // Add an event listener to ech of the .panels
+// for(let i = 0; i < panels.length; i++){
+//   panels[i].addEventListener('click', function(){
+//     for(let i = 0; i< panels.length; i++){
+//       panels[i].classList.remove("active");
+//     }
+//     this.classList.add("active");
+//   });
+// }
 
+panels.forEach((panel)=>{
+  panel.addEventListener('click',()=>{
+    removeActiveClasses();
+    panel.classList.add("active");
+  });
+});
 
-// Add an event listener to ech of the .panels
-
-for(let i = 0; i < panels.length; i++){
-  panels[i].addEventListener('click', function(){
-    for(let i = 0; i< panels.length; i++){
-      panels[i].classList.remove("active");
-    }
-    this.classList.add("active");
+function removeActiveClasses(){
+  panels.forEach((panel)=>{
+    panel.classList.remove('active');
   });
 }
